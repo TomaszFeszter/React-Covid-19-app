ead479e75bae08b8b5c26a539ed53746879969c9
TomaszFeszter <tomek.feszter.grupa@gmail.com>
2022-09-25 16:59:50 +0200

    fix plugin pwa version

--- vite.config.js
+++ vite.config.js
@@ -3,39 +3,8 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 import { VitePWA } from "vite-plugin-pwa";
 
-const pwaOptions = {
-  registerType: "autoUpdate",
-  mode: "development",
-  base: "/",
-  includeAssets: ["favicon.svg"],
-  injectRegister: "auto",
-  manifest: {
-    name: "PWA Router",
-    short_name: "PWA Router",
-    theme_color: "#ffffff",
-    icons: [
-      {
-        src: "pwa-192x192.png", // <== don't add slash, for testing
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/pwa-512x512.png", // <== don't remove slash, for testing
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "pwa-512x512.png", // <== don't add slash, for testing
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
-    ],
-  },
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslint(), VitePWA(pwaOptions), react()],
+  plugins: [eslint(), VitePWA(), react()],
   assetsInclude: ["**/**.otf"],
 });
